46d8615692426d65e50081e65281401419fa7cad
Richard Shin <ricshin@cs.berkeley.edu>
2018-03-15 04:12:30 +0800

    Add scripts/merge_indexed.py and start indexed_file library

---
+++ program_synthesis/datasets/indexed_file.py
@@ -0,0 +1,12 @@
+import struct
+
+def read_index(filename):
+    index = []
+    with open(filename) as index_file:
+        while True:
+            offset = index_file.read(8)
+            if not offset:
+                break
+            offset, = struct.unpack('<Q', offset)
+            index.append(offset)
+    return index
