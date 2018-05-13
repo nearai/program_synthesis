4b1c5dc402fefec0ea62f8208d638f1950f78d39
Marcelo Fornet <mfornet94@gmail.com>
2018-05-12 21:41:39 -0400

    Work on karel agent
    + Integration with new models
    + Implement interfaces for encoding decoding states
    + Implement policy behavior
    + Let rollout work as expected (without gradient updates)
    ! Missing implement `train_actor_critic`

--- program_synthesis/models/karel_agent.py
+++ program_synthesis/models/karel_agent.py
@@ -1,34 +1,45 @@
 """ Karel Agent - Reinforcement Learning
 
     More information on program_synthesis/models/karel-rl.md
+
+    Ideas:
+        + Hindsight Experience Replay
+        + Prioritized Experience Replay
+        + Meta learning (MAML, Reptile)
+        + Hierarchical methods
+
+    Reference:
+        Deep RL in parametrized action space
+        https://arxiv.org/pdf/1511.04143.pdf
 """
 import collections
-
 import io
+
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.utils.data
 from torch import optim
 
 from program_synthesis import datasets
 from program_synthesis.datasets import data
-from program_synthesis.datasets.karel import mutation
-from program_synthesis.datasets.karel import refine_env
+from program_synthesis.datasets.karel import refine_env, mutation
 from program_synthesis.datasets.karel.refine_env import MutationActionSpace
 from program_synthesis.models import karel_model
 from program_synthesis.models import prepare_spec
+from program_synthesis.models.karel_agent_model import ActionTypeModel, LocationParameterModel, KarelTokenParameterModel
 from program_synthesis.models.modules import karel
 from program_synthesis.models.modules import karel_common
-from program_synthesis.models.modules import karel_edit
 from program_synthesis.tools import saver
 
-DISCOUNT = 0.9
+DISCOUNT = 0.99
 EPSILON = 0.1
 ALPHA = 0.7
 
-
-#  https://arxiv.org/pdf/1511.04143.pdf
+VOCAB_SIZE = 30
+MAX_TOKEN_PER_CODE = 20
+TOKEN_EMBED_SIZE = 256
 
 
 class KarelEditEnv(object):
@@ -48,23 +59,25 @@ class KarelEditEnv(object):
         """
         :return: (Task, Observation/State)
         """
-        example = self.data_iter.next()  # returns list of 1 element.
-        self._cur_env = refine_env.KarelRefineEnv(example[0].input_tests)
+        sample = self.data_iter.next()  # returns list of 1 element.
+        self._cur_env = refine_env.KarelRefineEnv(sample[0].input_tests)
         obs = self._cur_env.reset()
-        input_grids, output_grids = karel_model.encode_io_grids(example)
+        input_grids, output_grids = karel_model.encode_io_grids(sample)
         return (input_grids, output_grids), self.prepare_obs(obs)
 
     @property
     def atree(self):
         return self._cur_env.atree
 
-    def prepare_tasks(self, tasks):
+    @staticmethod
+    def prepare_tasks(tasks):
         input_grids = torch.cat([t[0] for t in tasks], dim=0)
         output_grids = torch.cat([t[1] for t in tasks], dim=0)
         return input_grids, output_grids
 
-    def prepare_states(self, states):
-        padded_states = torch.empty(len(states), 20)
+    @staticmethod
+    def prepare_states(states):
+        padded_states = torch.zeros(len(states), MAX_TOKEN_PER_CODE)
         for idx, state in enumerate(states):
             padded_states[idx][:state.shape[1]] = state[0]
         return padded_states
@@ -97,66 +110,137 @@ class KarelEditPolicy(nn.Module):
         self.args = args
 
         self.io_encoder = karel_common.make_task_encoder(args)
-        self.code_encoder = karel.CodeEncoder(vocab_size, args)
+        self.code_encoder = karel.CodeEncoderAlternative(vocab_size, args)
 
-        # Action decoder
-        self.action_type = nn.Linear(100, 5)
-        self.action_pointer = karel_edit.ScaledDotProductPointer(100, 100)
-        self.action_token = nn.Linear(100, vocab_size, bias=False)
+        self.action_type = ActionTypeModel()
+
+        # Model to determine parameters
+        self.location = LocationParameterModel()
+        self.karel_token = KarelTokenParameterModel()
+
+        self._code_embed = None
+        self._state = None
 
     def encode_task(self, tasks):
         input_grids, output_grids = tasks
-        return self.io_encoder(input_grids, output_grids)
+        io_pair_enc = self.io_encoder(input_grids, output_grids)
+        max_value, _ = io_pair_enc.max(1)  # TODO: What about this final encoding
+        return max_value
 
-    def encode_code(self, code_state):
-        return self.code_encoder(code_state)
+    def encode_code(self, code):
+        """
+            :return: (code_embedded, code_latent_representation)
+        """
+        return self.code_encoder(code)
 
-    def action_value(self, task_state, code_state):
-        current_code_enc = self.encode_code(code_state)
-        state = torch.cat([task_state, current_code_enc.state])
-        act = self.action_type(state)
-        ptr = self.action_pointer(state)
-        token = self.action_token(state)
-        return act, ptr, token
+    def action_value(self, task_enc, code):
+        """ Compute expected reward of executing every action (without parameters)
+
+            Note:
+                Store the embedded code in the policy for picking parameters
+        """
+        self._code_embed, code_enc = self.encode_code(code)
+        self._state = torch.cat([task_enc, code_enc], 1)
+        act = self.action_type(self._state)
+        return act
+
+    def parameters_value(self, action):
+        """
+            Warning:
+                This method must be called after calling `action_value`
+        """
+        if action == mutation.ADD_ACTION:
+            pass
+        elif action == mutation.REMOVE_ACTION:
+            pass
+        elif action == mutation.REPLACE_ACTION:
+            pass
+        else:
+            raise NotImplementedError("Action parameters values not implemented yet.")
+
+    def forward(self):
+        pass
 
 
-class KarelAgent(object):
+class KarelAgent:
     def __init__(self, env, args):
         self.vocab = env.vocab
         self.env = env
-        self.task_state = None
+        self.task_enc = None
 
-        self.build_model(args)
-
-    def build_model(self, args):
-        self.model = KarelEditPolicy(30, args)
+        # Build model
+        self.model = KarelEditPolicy(VOCAB_SIZE, args)
         self.criterion = nn.MSELoss()
         self.optimizer = optim.Adam(self.model.parameters(), lr=args.lr)
 
     def set_task(self, task):
-        self.task_state = self.model.encode_task(task)
+        self.task_enc = self.model.encode_task(task)
 
-    def select_action(self, state, epsilon_greedy):
-        action_space = MutationActionSpace(atree=self.env.atree)
-        if True or (epsilon_greedy is not None and np.random.random() < epsilon_greedy):
-            action = action_space.sample()
+    @staticmethod
+    def get_distribution_from_values(action_value, mode='linear'):
+        if mode == "linear":
+            act_dist = action_value / action_value.sum()
+        elif mode == "softmax":
+            act_dist = torch.exp(action_value)
+            act_dist /= act_dist.sum()
         else:
-            _, action = self.best_action_value([state])
+            raise ValueError(f'Mode {mode} is invalid')
 
-        return action
+        return act_dist
 
-    def action_value(self, tasks, states):
+    def select_action(self, code, epsilon_greedy):
+        """ Select action (with parameters)
+
+            Note:
+                Call `set_task` first
+        """
+        action_space = MutationActionSpace(atree=self.env.atree)
+
+        # TODO[IMPLEMENT_ACTIONS]: This mask is to select only implemented actions
+        mask = torch.Tensor([1., 1., 1., 0., 0., 0., 0., 0.])
+
+        if epsilon_greedy is not None and np.random.random() < epsilon_greedy:
+            while True:
+                # Select action randomly
+                act_dist = mask
+                act_dist /= act_dist.sum()
+
+                action_id = np.random.choice(np.arange(8), p=act_dist.numpy().ravel())
+
+                # Select parameters randomly
+                params = action_space.sample_parameters(action_id)
+
+                if params is not None:
+                    return action_id, params
+        else:
+            action_value = self.model.action_value(self.task_enc, code)
+            act_dist = self.get_distribution_from_values(action_value, mode='softmax')
+
+            act_dist = act_dist * mask
+            act_dist /= act_dist.sum()
+
+            while True:
+                action_id = np.random.choice(np.arange(8), p=act_dist.numpy().ravel())
+
+                # Select parameters randomly (TODO: Select parameter according to learned parameter values network)
+                params = action_space.sample_parameters(action_id)
+
+                if params is not None:
+                    return action_id, params
+
+    def action_value(self, tasks, code):
+        """ Determine the value of each action (without parameters)
+        """
         tasks_encoded = self.model.encode_task(tasks)
-        return self.model.action_value(tasks_encoded, states)
+        return self.model.action_value(tasks_encoded, code)
 
-    def best_action_value(self, tasks, states):
-        input_grids, output_grids = tasks
-        task_state = self.model.encode(input_grids, output_grids)
-        (action_edit_values, action_position_values, action_token_values) = self.model.action_value(task_state, states)
-        edit_prob, edit_value = action_edit_values[0].max(1)
-        position_prob, position_value = action_position_values[0].max(1)
-        token_prob, token_value = action_token_values.values[0].max(1)
-        return (edit_prob, position_prob, token_prob), (edit_value, position_value, token_value)
+    def best_action_value(self, tasks, code):
+        """ Determine value of best action in current state
+        """
+        task_state = self.model.encode_task(tasks)
+        action_values = self.model.action_value(task_state, code)
+        best_val, _ = action_values.max(dim=1)
+        return best_val
 
     def train(self, tasks, states, actions, targets):
         self.optimizer.zero_grad()
@@ -193,6 +277,7 @@ class ReplayBuffer(object):
         return [self.buffer[idx] for idx in index]
 
 
+# TODO: Change `state` name for `code`
 class StepExample(collections.namedtuple('StepExample', ['task', 'state', 'action', 'reward', 'new_state'])):
     def __str__(self):
         buff = io.StringIO()
@@ -205,20 +290,21 @@ class StepExample(collections.namedtuple('StepExample', ['task', 'state', 'actio
 
 
 def rollout(env, agent, epsilon_greedy, max_rollout_length):
-    eps = EPSILON if epsilon_greedy else None
-    task, state = env.reset()
-    agent.set_task(task)
-    experience = []
-    success = False
-    for _ in range(max_rollout_length):
-        action = agent.select_action(state, eps)
-        new_state, reward, done, _ = env.step(action)
-        experience.append(StepExample(task, state, action, reward, new_state))
-        if done:
-            success = True
-            break
-        state = new_state
-    return success, experience
+    with torch.no_grad():
+        eps = EPSILON if epsilon_greedy else None
+        task, state = env.reset()
+        agent.set_task(task)
+        experience = []
+        success = False
+        for _ in range(max_rollout_length):
+            action = agent.select_action(state, eps)
+            new_state, reward, done, _ = env.step(action)
+            experience.append(StepExample(task, state, action, reward, new_state))
+            if done:
+                success = True
+                break
+            state = new_state
+        return success, experience
 
 
 class PolicyTrainer(object):
@@ -229,18 +315,21 @@ class PolicyTrainer(object):
         self.env = env
 
     def train_actor_critic(self, batch):
+        # TODO: Implement train actor critic
+
         tasks = self.env.prepare_tasks([ex.task for ex in batch])
         states = self.env.prepare_states([ex.state for ex in batch])
         new_states = self.env.prepare_states([ex.new_state for ex in batch])
         actions = self.env.prepare_actions([ex.action for ex in batch])
+
         targets = np.zeros(len(batch))
-        # prepare_batch(batch)
         values = self.critic.action_value(tasks, states)
-        new_value, _ = self.actor.best_action_value(new_states)
+        new_value = self.actor.best_action_value(tasks, new_states)
         for idx, ex in enumerate(batch):
             Q_s_a = values[idx][ex.action]
             new_Q_s_a = 0 if ex.reward == 0 else (ex.reward + DISCOUNT * new_value[idx])
             targets[idx] = Q_s_a + ALPHA * (new_Q_s_a - Q_s_a)
+
         self.critic.train(tasks, states, actions, targets)
 
     def train(self):
@@ -269,14 +358,14 @@ def main():
     agent_cls = KarelAgent
     env = KarelEditEnv()
 
-    success, experience = rollout(env, agent_cls(env, args), False, 10)
-
-    for x in experience:
-        print("*****************")
-        print(x)
-
-    env.reset()
-    exit(0)
+    # success, experience = rollout(env, agent_cls(env, args), False, 10)
+    #
+    # for x in experience:
+    #     print("*****************")
+    #     print(x)
+    #
+    # env.reset()
+    # exit(0)
 
     trainer = PolicyTrainer(args, agent_cls, env)
     trainer.train()
